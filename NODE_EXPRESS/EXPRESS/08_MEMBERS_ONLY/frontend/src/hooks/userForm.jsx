27e39aef8ac154ec262105a61d52ad166dbe3bb8
jnunez2301 <jmoises.n23@gmail.com>
2023-10-29 14:03:24 +0100

    Update userForm.jsx

--- NODE_EXPRESS/EXPRESS/08_MEMBERS_ONLY/frontend/src/hooks/userForm.jsx
+++ NODE_EXPRESS/EXPRESS/08_MEMBERS_ONLY/frontend/src/hooks/userForm.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import  { useState } from 'react'
 
 const userForm = () => {
 
+    const [ inputValue, setInputValue ] = useState('');
+    const [ formData, setFormData ] = useState();
+
+    const handleForm = (event) =>{
+        event.preventDefault();
+        setNew
+    }
     
+    const onInputChange = (event) =>{
+        const { name, value } = event.target;
+        setFormData({...formData, [name]: value});
+    }
+
+    
+
+
   return (
     <div>userForm</div>
   )
