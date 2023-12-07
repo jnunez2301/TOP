522f85801f000a403d7aef4a43d3e3634410232e
jnunez2301 <jmoises.n23@gmail.com>
2023-12-07 22:42:13 +0100

    learn to animate in 15mins lmao

---
+++ CSS_ANIMATIONS/DIC_07_START/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="container"> 
+       <div className="app"></div>
+       <div className="app-2"></div>
+       <div className="app-3"></div>
+       <div className="app-4"></div>
+    </div>
+    )
+}
+
+export default App
