7f8e299275e82ca8799b8b0cb030d4a20bab46be
jnunez2301 <jmoises.n23@gmail.com>
2023-12-09 20:48:06 +0100

    button

--- STUDY/REACT_NATIVE/01_INTRO_EXPO/App.js
+++ STUDY/REACT_NATIVE/01_INTRO_EXPO/App.js
@@ -1,22 +1,22 @@
-import { View, Text, Image, ImageBackground } from 'react-native';
+import { View, Text, Image, ImageBackground, ScrollView, Button } from 'react-native';
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default App = () => {
+
+  const handlePress = () => {
+    console.log('hi');
+  }
+
   return (
     <View style={{
       flex: 1,
       backgroundColor: "plum",
       padding: 60
     }}>
-      <Text>
-        <Text style={{color: 'white'}}>Hello </Text>World
-      </Text>
-      {/* <Image source={logoImg} style={{width: 300, height: 300}} />
-      <Image source={{uri: 'https://picsum.photos/200/200'}} style={{width: 300, height: 300}} /> */}
-      <ImageBackground source={logoImg}
-      style={{flex: 1}}>
-        <Text>IMAGE TEXT</Text>
-      </ImageBackground>
+      <Button
+      title='Press' 
+      onPress={handlePress}
+      color="midnightblue"/>
     </View>
   )
 }
