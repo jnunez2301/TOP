1ce470962b2ca16acbcf239a0b7ee4435b1e5c43
jnunez2301 <jmoises.n23@gmail.com>
2023-12-09 20:59:58 +0100

    Pressable

--- STUDY/REACT_NATIVE/01_INTRO_EXPO/App.js
+++ STUDY/REACT_NATIVE/01_INTRO_EXPO/App.js
@@ -1,22 +1,41 @@
-import { View, Text, Image, ImageBackground, ScrollView, Button } from 'react-native';
+import { View, Text, Image, Button, Pressable } from 'react-native';
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default App = () => {
-
   const handlePress = () => {
-    console.log('hi');
+    console.log('onPress');
   }
-
+  const handlePressIn = () => {
+    console.log('onPressIn');
+  }
+  const handlePressOut = () => {
+    console.log('onPressOut');
+  }
+  const handleLongPress = () => {
+    console.log('long press');
+  }
+  
   return (
     <View style={{
       flex: 1,
       backgroundColor: "plum",
-      padding: 60
+      padding: 60,
+      
     }}>
       <Button
       title='Press' 
       onPress={handlePress}
       color="midnightblue"/>
+      <Pressable onPressIn={handlePressIn}>
+        <Image source={logoImg} style={{width: 200,
+        height: 200}} />
+      </Pressable>
+      <Pressable onLongPress={handleLongPress}>
+      <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam similique, rem officiis, alias aliquam harum optio tempora ratione vitae, recusandae voluptas? Sint fugiat quam fugit veritatis commodi voluptates debitis sunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat repellendus delectus esse commodi id reiciendis, nihil eligendi. Odit magni iste amet fugit, ipsum hic placeat officiis tempora cupiditate facilis exercitationem.</Text>
+      </Pressable>
+      <Pressable onPressOut={handlePressOut}>
+        <Text>Press Out</Text>
+      </Pressable>
     </View>
   )
 }
