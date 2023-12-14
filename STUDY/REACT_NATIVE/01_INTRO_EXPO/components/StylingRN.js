d84096f8bb3dbeb57b4defe1ec5a7b4b1505bfcf
jnunez2301 <jmoises.n23@gmail.com>
2023-12-14 19:16:46 +0100

    Update StylingRN.js

--- STUDY/REACT_NATIVE/01_INTRO_EXPO/components/StylingRN.js
+++ STUDY/REACT_NATIVE/01_INTRO_EXPO/components/StylingRN.js
@@ -8,7 +8,10 @@ export const StylingRN = () => {
   )
 }
 
-const styles = StyleSheet.create({
+// You can also export it to use it everywhere
+// You can do it without the StyleSheet is possible 
+// But it doesn't provide autocompletion
+export const styles = StyleSheet.create({
     title: {
         color: 'white'
     }
