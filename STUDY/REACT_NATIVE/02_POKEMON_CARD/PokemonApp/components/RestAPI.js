45c834137c732e5c16d9e4cb0a5e84ba7ee29ae4
jnunez2301 <jmoises.n23@gmail.com>
2023-12-17 16:07:21 +0100

    core concepts of REST_API

--- STUDY/REACT_NATIVE/02_POKEMON_CARD/PokemonApp/components/RestAPI.js
+++ STUDY/REACT_NATIVE/02_POKEMON_CARD/PokemonApp/components/RestAPI.js
@@ -1,24 +1,101 @@
 import { useEffect, useState } from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, ActivityIndicator, SafeAreaView, StatusBar, TextInput, Button} from 'react-native';
 
 export const RestAPI = () => {
 
   const [postList, setPostList] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [isPosting, setIsPosting] = useState(false);
+  const [error, setError] = useState("");
   
   const fetchData = async (limit = 10) => {
-      const baseURL = `https://jsonplaceholder.typicode.com/posts?limit=${limit}`;
+      try{
+        const baseURL = `https://jsonplaceholder.typicode.com/posts?limit=${limit}`;
       const response = await fetch(baseURL);
       const data = await response.json();
       setPostList(data);
+      setIsLoading(false);
+      setError("")
+      }catch(error){
+        console.log(error);
+        setIsLoading(false);
+        setError("Failed to fetch Post List");
+      }
   };
+  const handleRefresh = () => {
+    setRefreshing(true);
+    fetchData(20);
+    setRefreshing(false);
+  }
   
   useEffect(() =>{
       fetchData()
     },[])
 
+  if(isLoading){
+      return (
+        <SafeAreaView style={styles.loadingContainer}>
+          <ActivityIndicator size='large' color="#0000ff" />
+          <Text>Loading...</Text>
+        </SafeAreaView>
+      )
+    }
+
+  const addPost = async() => {
+    setIsPosting(true);
+    try{
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+          method: 'post',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            title: postTitle,
+            body: postBody
+          })
+        }
+        );
+      const newPost = await response.json();
+      setPostList([newPost, ...postList]);
+      setPostTitle("");
+      setPostBody("");
+      setIsPosting(false);
+      setError("");
+    }catch(error){
+      console.error(error);
+      setError('Failed to Post');
+    }
+  }
 
   return (
+    <>
+    {
+      error ? 
+      <View style={styles.errorContainer}>
+        <Text style={styles.errorText}>{error}</Text>
+      </View> : 
+    <>
+    <View style={styles.inputContainer}>
+      <TextInput 
+      style={styles.input}
+      onChangeText={setPostTitle}
+      placeholder='Post title' value={postTitle}/>
+
+      <TextInput
+      style={styles.input}
+      placeholder='Post message'
+      onChangeText={setPostBody}
+      value={postBody}/>
+      <Button 
+      title={isPosting ? 'Adding...' : 'Post'} 
+      onPress={addPost}
+      disabled={isPosting}/>
+    </View>
     <View>
         <FlatList 
         data={postList}
@@ -34,8 +111,13 @@ export const RestAPI = () => {
         ListEmptyComponent={<Text>No Posts Found</Text>}
         ListHeaderComponent={<Text style={styles.headerText}>Post list</Text>}
         ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
+        refreshing={refreshing}
+        onRefresh={handleRefresh}
         />
     </View>
+    </>
+    }
+    </>
   )
 }
 
@@ -119,5 +201,32 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     textAlign: 'center',
     fontSize: 24
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: StatusBar.currentHeight
+  },
+  inputContainer: {
+    padding: 10,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 10
+  },
+  errorContainer: {
+   backgroundColor: "#FFC0CB",
+   padding: 16,
+   borderRadius: 8,
+   borderWidth: 1,
+   margin: 16,
+   alignItems: 'center'
+  },
+  errorText: {
+    color: "#D8000C",
+    fontSize: 16,
+    textAlign: "center"
   }
 });
